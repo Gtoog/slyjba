62e01856e6d39ad50617036f79b03f49844e034c
filin <filin@GTOOG>
2024-09-15 10:04:27 +0300

    Добавьте файлы проекта.

---
+++ demon/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+
+
+class Programm
+{
+    static void Main()
+    {
+        string exePath = @"C:\Users\filin\source\repos\demon\demon\Program.cs";
+
+        SetStartupRegistry(exePath);
+
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        watcher.Path = @"C:\PathToMonitor";
+        watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+        watcher.Filter = "*.txt";
+        watcher.Created += OnChanged;
+        watcher.Deleted += OnChanged;
+        watcher.Changed += OnChanged;
+        watcher.EnableRaisingEvents = true;
+
+        Console.WriteLine("Нажмите q чтобы завершить программу");
+        while (Console.Read() != 'q') ;
+    }
+
+    private static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        string logMessage = $"{e.ChangeType}: {e.FullPath} at {DateTime.Now}";
+        File.AppendAllText(@"C:\PathToLog\log.txt", logMessage + Environment.NewLine);
+    }
+    private static void SetStartupRegistry(string exePath)
+    {
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+        key.SetValue("demon", exePath);
+    }
+}
